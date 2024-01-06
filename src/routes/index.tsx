e32cab2a674ef41b64ac34aa1b9f682270d1082f
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-01-06 15:40:27 +0700

    Create login page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Homepage } from "../pages";
+import { Homepage, LoginPage } from "../pages";
 
 function Router() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Homepage />} />
+				<Route path="/login" element={<LoginPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
