613eeddb5c96603ec3d7f94e74f1d33b8767634c
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-02-05 04:08:56 +0700

    Create main layout with navigation bar

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import { NavigationBar } from "@/components";
+import React from "react";
+
+type MainLayoutProps = {
+	children: React.ReactNode;
+};
+
+export default function MainLayout({ children }: MainLayoutProps) {
+	return (
+		<React.Fragment>
+			<NavigationBar />
+			{/* <main>{children}</main> */}
+		</React.Fragment>
+	);
+}
