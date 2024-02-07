91a6c0fe2473a522ae1d60f5a367a0ad1373b251
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-02-08 03:50:40 +0700

    Add header of homepage

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -1,20 +1,27 @@
-import { Button } from "@/components";
-import { useTransactionQuery } from "@/services/query/transaction.query";
-import useAuthStore from "@/store/useAuthStore";
+import React from "react";
+import { SearchBar } from "@/components";
+import { useNavigate } from "react-router-dom";
 
 export default function Homepage() {
-	const { isLoading, data } = useTransactionQuery({ refCode: "abc-123" });
-	const { isAuthenticated } = useAuthStore();
+	const navigate = useNavigate();
 
-	if (isLoading) {
-		return <p>Loading...</p>;
+	function handleSearch(searchQuery: string): void {
+		navigate(`/search?q=${searchQuery}`);
+		return;
 	}
 
 	return (
-		<>
-			<p>Hello World RefCode: {data?.refCode}</p>
-			<Button message={"ทักทาย"} />
-			<p>สวัสดี ไอ้หน้าหี {JSON.stringify(isAuthenticated)}</p>
-		</>
+		<React.Fragment>
+			<header className="max-w-4xl my-0 mx-auto p-6 w-full pt-32 lg:pt-44 text-center">
+				<h1 className="text-4xl lg:text-5xl font-bold text-[#090b17]">Build your next idea even faster</h1>
+				<p className="mt-8 text-base lg:text-xl font-medium text-[#676e8b]">
+					FundO is Crowdfunding systems that use blockchain technology typically involve a project creator setting a
+					funding goal and backers contributing to the project using cryptocurrency.
+				</p>
+				<div className="mt-10">
+					<SearchBar placeholder="Enter your interested project" buttonText="Let's See!" action={handleSearch} />
+				</div>
+			</header>
+		</React.Fragment>
 	);
 }
