40f5fb80126ef812988561f8c5479189c4166441
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-02-08 03:50:27 +0700

    Create searchbar component in homepage

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as LoginForm } from "./Form/LoginForm";
 export { default as NavigationBar } from "./Bar/Navigation/NavigationBar";
+export { default as SearchBar } from "./Bar/Search/SearchBar";
