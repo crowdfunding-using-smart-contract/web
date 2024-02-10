815ed094eff4fe969e04be1bf76d27e97f180a08
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-02-11 01:47:22 +0700

    Fix create project button structer

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { default as LoginForm } from "./Form/LoginForm";
 export { default as NavigationBar } from "./Bar/Navigation/NavigationBar";
 export { default as SearchBar } from "./Bar/Search/SearchBar";
 export { default as ProjectCard } from "./Card/ProjectCard";
-export { default as CreateProjectButton } from "./Button/CreateProjectButton";
+export { default as CreateProjectButton } from "./Button/CreateProject/CreateProjectButton";
