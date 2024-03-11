c1f439df0384c66ed27ddc388b15706bbe40eaac
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-03-11 18:17:14 +0700

    Override tailwind colors and add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
 export default {
 	content: ["./src/**/*.tsx"],
 	theme: {
 		extend: {},
+		colors: {
+			primary: "rgb(var(--color-primary) / <alpha-value>)",
+			"font-primary": "rgb(var(--color-text-primary) / <alpha-value>)",
+			"light-blue": colors.sky,
+			"warm-gray": colors.stone,
+			"true-gray": colors.neutral,
+			"cool-gray": colors.gray,
+			"blue-gray": colors.slate,
+			...colors,
+		},
 	},
 	plugins: [],
 };
