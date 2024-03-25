5f6a47e1d75c97389d08a1de6ea41b880bc44ccc
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-03-26 03:42:50 +0700

    chore: add relative time plugin in dayjs

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import i18nOptions from "./i18n/options.ts";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
@@ -11,6 +13,7 @@ import "@blocknote/core/fonts/inter.css";
 import "@blocknote/react/style.css";
 import "./index.css";
 
+dayjs.extend(relativeTime);
 i18n.use(initReactI18next).init(i18nOptions);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
